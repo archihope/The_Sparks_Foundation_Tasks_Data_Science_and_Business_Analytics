826778f3da3a2577e1805f2b91782d041f68dfce
Archi <163654990+archihope@users.noreply.github.com>
2024-03-18 12:00:41 +0530

    Add files via upload

---
+++ The_Sparks_Foundation_Supervised_ML_Task_1_Archi.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+
+# Reading the Data
+url = "http://bit.ly/w-data"
+data = pd.read_csv(url)
+print("----Data uploaded Successfully----")
+
+print(data.head())
+
+print(data.isnull == True)
+
+sns.set_style('darkgrid')
+sns.scatterplot(y= data['Scores'], x= data['Hours'])
+plt.title('Marks Vs Study Hours',size=20)
+plt.ylabel('Marks Percentage', size=12)
+plt.xlabel('Hours Studied', size=12)
+plt.show()
+
+sns.regplot(x= data['Hours'], y= data['Scores'])
+plt.title('Regression Plot',size=20)
+plt.ylabel('Marks Percentage', size=12)
+plt.xlabel('Hours Studied', size=12)
+plt.show()
+print(data.corr())
+
+X = data.iloc[:, :-1].values
+y = data.iloc[:, 1].values
+
+# Spliting the Data in two
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
+
+regression = LinearRegression()
+regression.fit(train_X, train_y)
+print("---------Model Trained---------")
+
+pred_y = regression.predict(val_X)
+prediction = pd.DataFrame({'Hours': [i[0] for i in val_X], 'Predicted Marks': [k for k in pred_y]})
+
+compare_scores = pd.DataFrame({'Actual Marks': val_y, 'Predicted Marks': pred_y})
+
+
+plt.scatter(x=val_X, y=val_y, color='blue')
+plt.plot(val_X, pred_y, color='Black')
+plt.title('Actual vs Predicted', size=20)
+plt.ylabel('Marks Percentage', size=12)
+plt.xlabel('Hours Studied', size=12)
+plt.show()
+
+
+# Calculating the accuracy of the model
+print('Mean absolute error: ',mean_absolute_error(val_y,pred_y))
+
+hours = [9.25]
+answer = regression.predict([hours])
+print("Score = {}".format(round(answer[0],3)))
+
